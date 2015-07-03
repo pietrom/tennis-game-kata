ffa6374b02a7ee94bc49e883ffbbe149c3b6a6e8
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2015-07-03 16:25:15 +0200

    Extracted function createCurrentGameScore

--- tennis.js
+++ tennis.js
@@ -1,11 +1,16 @@
+function createCurrentGameScore(pl1, pl2) {
+	var currentGameScore = {};
+	currentGameScore[pl1] = 0;
+	currentGameScore[pl2] = 0;
+	return currentGameScore;
+};
+
 var tennis = {	
 	whoami: 'The Tennis Game kata',
 	Game: function(player1, player2) {
 		this.player1 = player1;
 		this.player2 = player2;
-		this.currentGameScore = {};
-		this.currentGameScore[this.player1] = 0;
-		this.currentGameScore[this.player2] = 0;
+		this.currentGameScore = createCurrentGameScore(player1, player2);
 	}
 };
 

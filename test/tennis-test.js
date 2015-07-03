cb66ec2cffd562c7769f2c74f73fc069693c91d2
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2015-07-03 16:28:04 +0200

    Extracted assertScoreIs

--- test/tennis-test.js
+++ test/tennis-test.js
@@ -1,20 +1,23 @@
 var should = require('should');
 var tennis = require('../tennis');
 
-describe('Tennis', function(){
+function assertScoreIs(score, v1, v2) {
+	score.Alberto.should.equal(v1);
+	score.Stefano.should.equal(v2);
+};
+
+describe('Tennis', function() {
   describe('setup', function() {
     it('initial game score is 0 for both players', function() {
       var game = new tennis.Game('Alberto', 'Stefano');
 	  var score = game.getCurrentGameScore();
-	  score.Alberto.should.equal(0);
-	  score.Stefano.should.equal(0);
+	  assertScoreIs(score, 0, 0);
     });
 	
 	it('initial set score is 0 for both players', function() {
       var game = new tennis.Game('Alberto', 'Stefano');
 	  var score = game.getCurrentSetScore();
-	  score.Alberto.should.equal(0);
-	  score.Stefano.should.equal(0);
+	  assertScoreIs(score, 0, 0);
     });
   });
   
@@ -23,8 +26,7 @@ describe('Tennis', function(){
 		var game = new tennis.Game('Alberto', 'Stefano');
 		game.scorePoint('Alberto');
 		var score = game.getCurrentGameScore();
-		score.Alberto.should.equal(15);
-		score.Stefano.should.equal(0);
+		assertScoreIs(score, 15, 0);
 	});
   });
 })
